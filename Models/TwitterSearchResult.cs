60d2a3626e3f6ead2fe51a2219acfb42a3249716
James Darling <jamesdarling@microsoft.com>
2021-05-23 15:40:38 -0500

    Create repo with starting code for API.

---
+++ Models/TwitterSearchResult.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace HateDetector.API.Models
+{
+    /// <summary>
+    /// Root class of data for search results response from Twitter API
+    /// </summary>
+    public class TwitterSearchResult
+    {
+        public List<Result> Results { get; set; }
+
+        public SearchResultMetadata Metadata { get; set; }
+    }
+
+    /// <summary>
+    /// Individual Tweet found durring search using Twitter API
+    /// </summary>
+    public class Result
+    {
+        [JsonProperty("id")]
+        public string TweetId { get; set; }
+
+        [JsonProperty("public_metrics")]
+        public PublicMetrics Metrics { get; set; }
+
+        [JsonProperty("text")]
+        public string TweetText { get; set; }
+    }
+
+    /// <summary>
+    /// The metrics for an individual Tweet returned in search
+    /// </summary>
+    public class PublicMetrics
+    {
+        [JsonProperty("retweet_count")]
+        public int Retweets { get; set; }
+        
+        [JsonProperty("reply_count")]
+        public int Replies { get; set; }
+
+        [JsonProperty("like_count")]
+        public int Likes { get; set; }
+
+        [JsonProperty("quote_count")]
+        public int Quotes { get; set; }
+    }
+
+    /// <summary>
+    /// Model for Metadata for payload returned from searching the Twitter API
+    /// </summary>
+    public class SearchResultMetadata
+    {
+        [JsonProperty("newest_id")]
+        public string NewestTweetId { get; set; }
+
+        [JsonProperty("oldest_id")]
+        public string OldestTweetId { get; set; }
+
+        [JsonProperty("result_count")]
+        public int ResultCount { get; set; }
+    }
+
+}
